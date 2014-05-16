14fc0e296fad0a8fd8d804ed2081bc8d8e048c70
VCL1995 <lc63929211@gmail.com>
2014-05-16 15:15:12 -0400

    Since there is no function for click, the hover function can have the same effect on mobile devices.

--- js/functions.js
+++ js/functions.js
@@ -157,24 +157,25 @@ $(document).ready(function(){
 		$(".page"+presentPageNum).fadeIn();
 	})
 	// individual example icon on click effect
-	$(".example").click(function(){
-		var exampleId = "#" + $(this).attr('id');
-		var infoDisplayStatus = $(exampleId + " .info").css("display");
-		if(infoDisplayStatus == "none"){
+	//$(".example").click(function(){
+		//var exampleId = "#" + $(this).attr('id');
+		//var infoDisplayStatus = $(exampleId + " .info").css("display");
+		//if(infoDisplayStatus == "none"){
+			//$(exampleId + " .info").fadeIn("fast");
+		//}else{
+			//$(exampleId + " .info").fadeOut("fast");
+		//}
+	//})
+	// individual exmaple icon hover effect
+	$(".example").hover(
+		function() {
+			var exampleId = "#" + $(this).attr('id');
 			$(exampleId + " .info").fadeIn("fast");
-		}else{
+		}, function() {
+			var exampleId = "#" + $(this).attr('id');
 			$(exampleId + " .info").fadeOut("fast");
 		}
-	})
-	// individual exmaple icon hover effect
-	//$(".example").hover(
-	//	function() {
-	//		var exampleId = "#" + $(this).attr('id');
-	//		$(exampleId + " .info").fadeIn("fast");
-	//	}, function() {
-	//		$(exampleId + " .info").fadeOut("fast");
-	//	}
-	//)
+	)
 	// page button hover effect
 	$(".scroll").hover(
 		function(){
